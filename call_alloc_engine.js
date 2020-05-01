47fd2991a842c5b2779c5b23a4715970754e3100
sharonpamela <sharon.pamela.santana@gmail.com>
2020-05-01 13:50:31 -0700

    added registry key

--- call_alloc_engine.js
+++ call_alloc_engine.js
@@ -1,4 +1,20 @@
 const axios = require('axios');
+var regedit = require('regedit')
+import axios from 'axios';
+
+regedit.list('HKCU\\Environment', function(err, result) {
+    const email = result['HKCU\\Environment'].values.FRAME_USER_EMAIL.value
+        // returns:
+        // { type: 'REG_SZ', value: 'sharon.santana@nutanix.com' }
+    axios.get(`http://10.42.247.61/api/users/${email}`)
+        .then(response => {
+            console.log(response.data)
+        })
+        .catch(e => {
+            console.log(e);
+        });
+
+})
 
 // axios.get('http://10.42.247.61/api/users')
 //     .then(response => {
@@ -18,10 +34,10 @@ const axios = require('axios');
 //         console.log(e);
 //     });
 
-axios.get('http://10.42.247.61/api/users/sharon@nutanix.com')
-    .then(response => {
-        console.log(response.data)
-    })
-    .catch(e => {
-        console.log(e);
-    });
+// axios.get('http://10.42.247.61/api/users/sharon@nutanix.com')
+//     .then(response => {
+//         console.log(response.data)
+//     })
+//     .catch(e => {
+//         console.log(e);
+//     });
